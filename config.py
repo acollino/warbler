4e2c6b10648202f008917e434eb5c9fd34a3dd34
Alex Collino <arcollino@gmail.com>
2022-09-14 16:24:00 -0700

    edit config, env, app init for ease of testing

--- config.py
+++ config.py
@@ -4,14 +4,13 @@ from dotenv import load_dotenv
 
 # Get the path to the directory of this file
 base_directory = path.abspath(path.dirname(__file__))
-
-# Load the .env file found in the same directory
-load_dotenv(path.join(base_directory, ".env"))
+# The .env file found in that same directory will be loaded
 
 
 class Config:
     """Set the base configuration for Flask."""
 
+    load_dotenv(path.join(base_directory, ".env"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
     SECRET_KEY = environ.get("SECRET_KEY")
@@ -29,3 +28,17 @@ class ProdConfig(Config):
 
     SQLALCHEMY_ECHO = False
     DEBUG = False
+
+
+class TestConfig:
+    """Set the testing configuration for Flask."""
+
+    load_dotenv(path.join(base_directory, "test.env"))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SQLALCHEMY_ECHO = True
+    DEBUG = True
+    TESTING = True
+    # Per Springboard, don't have WTForms use CSRF at all, since it's a pain to test
+    WTF_CSRF_ENABLED = False
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import init_app
 
-app = init_app()
+# change to ProdConfig before deploying
+app = init_app("config.DevConfig")
 
 if __name__ == "__main__":
     app.run()
