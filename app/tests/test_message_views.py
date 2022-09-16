3987056f02e72c79d24667681cab0c929811ed15
Alex Collino <arcollino@gmail.com>
2022-09-16 11:23:43 -0700

    add test to delete a user's message

--- app/tests/test_message_views.py
+++ app/tests/test_message_views.py
@@ -37,6 +37,22 @@ class MessageViewTestCase(TestCase):
 
         db.session.commit()
 
+    def add_user(self, name):
+        """Add a user to the db for testing."""
+
+        user = User(email=f"{name}@test.com", username=name, password="HASHED_PASSWORD")
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def add_msg(self, message, id):
+        """Add a message to the db for testing."""
+
+        msg = Message(text=message, user_id=id)
+        db.session.add(msg)
+        db.session.commit()
+        return msg
+
     def test_add_message(self):
         """Can a user add a message?"""
 
@@ -49,6 +65,21 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    def test_delete_message(self):
+        """Can a user delete their message?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            msg_to_delete = self.add_msg("sample text", self.testuser.id)
+
+            resp = c.post(f"/messages/{msg_to_delete.id}/delete")
+            self.assertEqual(resp.status_code, 302)
+
+            msg_query = Message.query.one_or_none()
+            self.assertIsNone(msg_query)
 
     def tearDown(self):
         """Clear testing data from User and Message tables."""
