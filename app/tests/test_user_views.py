899524afa28f9258e9ad828d639813da04e36ae2
Alex Collino <arcollino@gmail.com>
2022-09-15 17:18:00 -0700

    implement user-view tests

--- app/tests/test_user_views.py
+++ app/tests/test_user_views.py
@@ -53,9 +53,8 @@ class UserViewsTestCase(TestCase):
 
             resp = c.post(f"/users/follow/{followed_by_user.id}")
 
-            # Make sure it redirects
             self.assertEqual(resp.status_code, 302)
-            self.assertEqual(self.testuser.is_following(followed_by_user), True)
+            self.assertTrue(self.testuser.is_following(followed_by_user))
 
     def test_followed_by_others(self):
         """Can a user be followed by another?"""
@@ -75,13 +74,84 @@ class UserViewsTestCase(TestCase):
 
             resp = c.post(f"/users/follow/{self.testuser.id}")
 
-            # Make sure it redirects
             self.assertEqual(resp.status_code, 302)
-            self.assertEqual(self.testuser.is_followed_by(follower_of_user), True)
+            self.assertTrue(self.testuser.is_followed_by(follower_of_user))
+
+    def test_not_following(self):
+        """Do the User-following functions work correctly when the users are not followers?"""
+
+        follower_of_user = User(
+            email="follower_of_test@test.com",
+            username="follower_of_test",
+            password="HASHED_PASSWORD",
+        )
+        not_follower = User(
+            email="not_follower@test.com",
+            username="not_follower",
+            password="HASHED_PASSWORD",
+        )
+
+        db.session.add_all([follower_of_user, not_follower])
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = follower_of_user.id
+
+            _ = c.post(f"/users/follow/{self.testuser.id}")
+
+            self.assertTrue(self.testuser.is_followed_by(follower_of_user))
+            self.assertFalse(self.testuser.is_followed_by(not_follower))
+            self.assertFalse(self.testuser.is_following(not_follower))
+
+    def test_invalid_signup(self):
+        """Does User creation fail successfully if improper credentials are given?"""
+
+        with self.client as c:
+            resp = c.post(
+                "/signup",
+                data={
+                    "username": "tester2",
+                    "email": "invalid email",
+                    "password": "password",
+                },
+            )
+
+            # Make sure it refreshes the form page rather than redirect
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Invalid email address.", resp.text)
+
+    def test_duplicate_signup(self):
+        """Does User creation fail successfully if duplicate credentials are given?"""
+
+        with self.client as c:
+            email_resp = c.post(
+                "/signup",
+                data={
+                    "username": "tester2",
+                    "email": "test@test.com",
+                    "password": "password",
+                },
+            )
+            self.assertEqual(email_resp.status_code, 200)
+            self.assertIn("That email is already registered.", email_resp.text)
+
+            username_resp = c.post(
+                "/signup",
+                data={
+                    "username": "testuser",
+                    "email": "newtest@test.com",
+                    "password": "password",
+                },
+            )
+
+            self.assertEqual(username_resp.status_code, 200)
+            self.assertIn("That username is already taken.", username_resp.text)
 
     def tearDown(self):
         """Clear testing data from User, Message, Follows tables."""
 
+        db.session.rollback()
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
